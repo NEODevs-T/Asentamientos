88706be30cc1b97cdc9b0642dd13c48a50557544
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2023-11-21 15:44:27 -0400

    Inicio de la apliacaión y Escaneo del DB

---
+++ Models/Division.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asentamientos.Models;
+
+public partial class Division
+{
+    public int IdDivision { get; set; }
+
+    public string? Dnombre { get; set; }
+
+    public string? Ddetalle { get; set; }
+
+    public bool? Destado { get; set; }
+
+    public virtual ICollection<Master> Masters { get; set; } = new List<Master>();
+}
---
+++ Models/Unidad1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asentamientos.Models;
+
+public partial class Unidad1
+{
+    public int IdUnidad { get; set; }
+
+    public string Unombre { get; set; } = null!;
+
+    public string? Udescri { get; set; }
+
+    public bool Uestado { get; set; }
+}
