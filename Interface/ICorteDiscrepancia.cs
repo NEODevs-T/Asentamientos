855da42c5ca64e59f6609f88132d684b76d2b6b3
Javier Taborda <javieralejandro0999@gmail.com>
2024-01-03 09:23:41 -0400

    Corrección en FiltroMaestra con roles y textbox

--- Interface/ICorteDiscrepancia.cs
+++ Interface/ICorteDiscrepancia.cs
@@ -1,11 +1,17 @@
+using Asentamientos.DTOs;
 using Asentamientos.Models;
 using Asentamientos.ModelsViews;
+using System;
 
 namespace Asentamientos.Interface
 {
     public interface ICorteDiscrepancia
     { 
         Task<List<Asentum>> GetAsentamientosFueraRango(string turno, string fecha, int idcentro );
+        Task<List<CorteDiscDTO>> GetCortesDiscrepancia(string turno, string fecha, int idcentro );
+        Task<object> AddCortesDiscrepancias(List<CorteDiscDTO>? cortes);
+        Task<List<CategoriaDTO>> GetCategorias();
     }
+
         
 }
